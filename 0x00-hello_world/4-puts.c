3d3ff1b1d28ecb5cfc18a1fcbd48021925f73681
Skidjared <chinedujared@gmail.com>
2022-12-14 08:32:07 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * main - prints a given phrase
- * Return: 0 if exited properly, non-zero otherwise
+ * main - "Programming is like building a multilingual puzzle, followed by a
+ * new line
+ * Return: Always 0.
  */
 
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
